0aaeafacbf1821142f1e9394931f3d145c068bd1
himusinggit <hbilluscoding@gmail.com>
2025-12-18 23:11:18 +0530

    Enhance channel statistics retrieval with total videos, views, subscribers, and likes

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,10 +8,62 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const totalVideos=await Video.countDocuments({owner:req.user._id});
+    const channelData=Video.aggregate([
+        {
+            $match:{
+                owner:req.user._id
+            }
+        },
+        {
+            $group:{
+                _id:null,
+                totalVideos:{$sum:1},
+                totalVideoViews:{$sum:"$views"}
+            }
+        }
+    ]);
+    const channelSubsribers=await Subscription.aggregate([
+        {
+            $match:{
+                channel:req.user._id
+            }
+        },
+        {
+            $group:{
+                _id:null,
+                totalSubscribers:{$sum:1}
+            }
+        }
+    ])
+    const userVideos=await Video.find({owner:req.user._id}).select("_id");
+    const channelLikes=await Like.aggregate([
+        {
+            $match:{
+                likedItemModel:'Video',
+                likedItem:{$in:userVideos.map(video=>video._id)}
+            }
+        },
+        {
+        $group:{
+            _id:null,
+            totalChannelLikes:{$sum:1}
+        }
+    }
+    ])
+    res.status(200).json(new ApiResponse(200,{channelVideos:channelData,channelLikes,channelSubsribers},"channel data retrieved successfully"))
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const channelVideos=Video.aggregate([
+        {
+            $match:{
+                owner:req.user._id
+            }
+        }
+    ])
+    res.status(200).json(new ApiResponse(200,channelVideos,"channel videos retrieved successfully"))
 })
 
 export {
